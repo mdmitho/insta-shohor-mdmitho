0d48cb02ad7fea695b764ecbcb3b0a06507d2782
Hossen <mdmitho199911@gmail.com>
2022-03-10 22:39:26 +0600

    like not repet done

--- js/app.js
+++ js/app.js
@@ -154,6 +154,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML=''
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -163,7 +164,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
+      
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
