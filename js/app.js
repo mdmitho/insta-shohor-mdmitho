a2c9375a663e598f29af77b9d0c86088e848f1ab
Hossen <mdmitho199911@gmail.com>
2022-03-10 22:28:02 +0600

    description 30 text show done

--- js/app.js
+++ js/app.js
@@ -28,7 +28,12 @@ const reportPost = (id) => {
 
 const displayContent = (text) => {
 
+if(text.length > 30){
   return text.length < 30 ?  'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+}
+else{
+  return text
+}
 
   
 };
